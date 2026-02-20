d86308da046a5461d84d4774d59106316c126492
Jitse De Smet <jitse@reedonk8.be>
2026-02-20 17:12:48 +0100

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+  import type {LayoutProps} from "./$types";
+  let { children }: LayoutProps = $props();
+</script>
+
+{@render children?.()}
+
+<style>
+    :global {
+        body {
+            margin: 0;
+        }
+        p {
+            text-justify: auto;
+            text-align: justify;
+        }
+        * {
+            font-family: "Source Sans Pro", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 300;
+            font-style: normal;
+            font-variation-settings:
+                    "slnt" 0;
+            font-size: 1em;
+            /* can be enabled again color: var(--color-main-text); */
+        }
+        h1, h2, h3, h4, h5 {
+            padding: 0;
+            margin: 0;
+        }
+        h2 {
+            font-size: 2em;
+        }
+        h3 {
+            font-size: 1.5em;
+        }
+        button {
+            cursor: pointer;
+            background: none;
+            border: none;
+        }
+        div {
+            color: var(--color-text);
+        }
+        ul {
+            padding: 0;
+            margin: 0;
+            list-style-type: none;
+        }
+        a {
+            color: var(--color-text);
+        }
+        html {
+            background-color: var(--color-bg);
+            --color-bg: white;
+            --color-gray-canvas: #F6F6F6;
+            --color-text: black;
+            --color-opposite-text: white;
+            --color-primary: #2194f3;
+            --color-primary-dark: #1045a1;
+            --color-danger: #E65244;
+            --color-success: #49D793;
+            --color-warning: #f37f21;
+            --color-complete: #A599C6;
+            --color-info: #42a4f5;
+        }
+
+        /* https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Reference/Roles/tooltip_role */
+        [role="tooltip"], .hide-tooltip.hide-tooltip.hide-tooltip + [role="tooltip"] {
+            visibility: hidden;
+            position: absolute;
+            top: 1rem;
+            left: 1rem;
+            background: var(--color-gray-canvas);
+            color: black;
+        }
+        [aria-describedby]:hover, [aria-describedby]:focus {
+            position: relative;
+        }
+        [aria-describedby]:hover  [role="tooltip"], [aria-describedby]:focus  [role="tooltip"] {
+            transition-delay: 0.5s;
+            visibility: visible;
+        }
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,99 @@
+<script lang="ts">
+  import {innerWidth} from 'svelte/reactivity/window';
+
+  let screenWidth = $derived(innerWidth.current ?? 600);
+  let smallScreen = $derived<boolean>(screenWidth < 550);
+</script>
+
+
+<div class="header">
+    <a href="/" class="no-style"><h1>SPARQL Engine & Parser composability</h1></a>
+</div>
+
+
+<footer>
+    <a href="https://github.com/jitsedesmet/demo-mixed-composability">
+        <svg height="2rem" aria-hidden="true" viewBox="0 0 24 24" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github v-align-middle">
+            <path d="M12 1C5.9225 1 1 5.9225 1 12C1 16.8675 4.14875 20.9787 8.52125 22.4362C9.07125 22.5325 9.2775 22.2025 9.2775 21.9137C9.2775 21.6525 9.26375 20.7862 9.26375 19.865C6.5 20.3737 5.785 19.1912 5.565 18.5725C5.44125 18.2562 4.905 17.28 4.4375 17.0187C4.0525 16.8125 3.5025 16.3037 4.42375 16.29C5.29 16.2762 5.90875 17.0875 6.115 17.4175C7.105 19.0812 8.68625 18.6137 9.31875 18.325C9.415 17.61 9.70375 17.1287 10.02 16.8537C7.5725 16.5787 5.015 15.63 5.015 11.4225C5.015 10.2262 5.44125 9.23625 6.1425 8.46625C6.0325 8.19125 5.6475 7.06375 6.2525 5.55125C6.2525 5.55125 7.17375 5.2625 9.2775 6.67875C10.1575 6.43125 11.0925 6.3075 12.0275 6.3075C12.9625 6.3075 13.8975 6.43125 14.7775 6.67875C16.8813 5.24875 17.8025 5.55125 17.8025 5.55125C18.4075 7.06375 18.0225 8.19125 17.9125 8.46625C18.6138 9.23625 19.04 10.2125 19.04 11.4225C19.04 15.6437 16.4688 16.5787 14.0213 16.8537C14.42 17.1975 14.7638 17.8575 14.7638 18.8887C14.7638 20.36 14.75 21.5425 14.75 21.9137C14.75 22.2025 14.9563 22.5462 15.5063 22.4362C19.8513 20.9787 23 16.8537 23 12C23 5.9225 18.0775 1 12 1Z"></path>
+        </svg>
+        <span>Source code</span>
+    </a>
+    <a href="https://traqula-resource.jitsedesmet.be/">
+        <svg height="1.7rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" style="fill: rgb(100,100,100)">
+            <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+            <path d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 288c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128z"/></svg>
+        <span>Paper</span>
+    </a>
+</footer>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Audiowide&family=Funnel+Display:wght@300..800&display=swap');
+    .no-style, .no-style:visited, .no-style:hover, .no-style:active {
+        text-decoration: none;
+        color: inherit;
+    }
+    footer {
+        display: flex;
+        gap: 1rem;
+        padding: 3px 10px;
+    }
+    footer a {
+        display: flex;
+        align-items: center;
+        vertical-align: center;
+        gap: 0.2rem;
+    }
+    h1 {
+        text-align: center;
+        font-family: "Audiowide", serif;
+        font-style: normal;
+        font-size: 3em;
+        font-weight: 400;
+        margin: 0 0 0.25em 0;
+    }
+    .header {
+        display: flex;
+        justify-content: space-between;
+    }
+    :global {
+        *:not(.yasge *) {
+            font-family: "Funnel Display", serif;
+            font-optical-sizing: auto;
+            font-weight: 300;
+            font-style: normal;
+        }
+        a, a:visited, a:hover, a:active { color: #0000ee; }
+        button, select {
+            appearance: none;
+            background-color: #fafbfc;
+            border: 1px solid rgba(27, 31, 35, 0.15);
+            border-radius: 6px;
+            box-shadow:
+                    rgba(27, 31, 35, 0.04) 0 1px 0,
+                    rgba(255, 255, 255, 0.25) 0 1px 0 inset;
+            box-sizing: border-box;
+            color: #24292e;
+            cursor: pointer;
+            display: inline-block;
+            font-size: 14px;
+            font-weight: 500;
+            line-height: 20px;
+            list-style: none;
+            padding: 6px 16px;
+            position: relative;
+            user-select: none;
+            -webkit-user-select: none;
+            touch-action: manipulation;
+            vertical-align: middle;
+            white-space: nowrap;
+            word-wrap: break-word;
+        }
+        .breakable {
+            white-space: pre-wrap; /* CSS3 */
+            white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
+            white-space: -pre-wrap; /* Opera 4-6 */
+            white-space: -o-pre-wrap; /* Opera 7 */
+            word-wrap: break-word; /* Internet Explorer 5.5+ */
+        }
+    }
+</style>
