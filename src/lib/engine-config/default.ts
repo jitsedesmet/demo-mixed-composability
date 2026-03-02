03abfc7ded27dc76589cfa1a07f8598358f7477a
Jitse De Smet <jitse@reedonk8.be>
2026-03-02 12:05:35 +0100

    add default and use github perma links

---
+++ src/lib/engine-config/default.ts
@@ -0,0 +1,6 @@
+import type {FileDescription} from "./engineConfTypes";
+
+export const defaultConfig: FileDescription = {
+  name: "config-default.json",
+  body: new URL("https://github.com/comunica/comunica/raw/d222adb412a5605dad68414c4a7ee953dae13d84/engines/config-query-sparql/config/config-default.json"),
+}
